c0e56aafa1cb84877f7cfae55501797acb9852b4
Aure7138 <wyaure@163.com>
2024-09-15 18:27:13 +0800

    Fix pgrep -x issue due to FreeBSD 19-char proc name limit

--- crontab_monitor.sh
+++ crontab_monitor.sh
@@ -8,7 +8,7 @@ xray_dir="$HOME/xray"
 # 判断目录是否存在
 if [ -d "$xray_dir" ]; then
     # 添加新的保活任务
-    (crontab -l 2>/dev/null; echo "*/5 * * * * if ! pgrep -x \"xray\" > /dev/null; then cd $xray_dir && nohup ./xray run -c config.json > /dev/null 2>&1 & fi") | crontab -
+    (crontab -l 2>/dev/null; echo "*/5 * * * * if ! pgrep -f \"./xray run -c config.json\" > /dev/null; then cd $xray_dir && nohup ./xray run -c config.json > /dev/null 2>&1 & fi") | crontab -
 
     # 添加新的重启后自动启动任务
     (crontab -l 2>/dev/null; echo "@reboot pkill -f \"./xray run -c config.json\" && cd $xray_dir && nohup ./xray run -c config.json > /dev/null 2>&1 &") | crontab -
@@ -17,6 +17,6 @@ fi
 hysteria2_dir="$HOME/hysteria2"
 
 if [ -d "$hysteria2_dir" ]; then
-    (crontab -l 2>/dev/null; echo "*/5 * * * * if ! pgrep -x \"hysteria-freebsd-amd64\" > /dev/null; then cd $hysteria2_dir && nohup ./hysteria-freebsd-amd64 server -c config.yaml > /dev/null 2>&1 & fi") | crontab -
+    (crontab -l 2>/dev/null; echo "*/5 * * * * if ! pgrep -f \"./hysteria-freebsd-amd64 server -c config.yaml\" > /dev/null; then cd $hysteria2_dir && nohup ./hysteria-freebsd-amd64 server -c config.yaml > /dev/null 2>&1 & fi") | crontab -
     (crontab -l 2>/dev/null; echo "@reboot pkill -f \"./hysteria-freebsd-amd64 server -c config.yaml\" && cd $hysteria2_dir && nohup ./hysteria-freebsd-amd64 server -c config.yaml > /dev/null 2>&1 &") | crontab -
 fi
